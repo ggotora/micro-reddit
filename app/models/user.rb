4e792821c84f5763157adaee370c1dfee1046b0c
Yors <jatbribiesca@gmail.com>
2020-11-16 15:59:54 -0600

    add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum: 2 }
+  validates :username, presence: true, length: { minimum: 3 }
+  validates :email, presence: true, uniqueness: true
 end
